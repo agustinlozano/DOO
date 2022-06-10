d7ea4691d2ca1789d8036493d0878f0f4bb31b96
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-06-09 21:03:49 -0300

    Edit main identation

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,38 +1,38 @@
 #include "matriz.h"
 
- void f(matriz<int> *x) {
-	cout << *x << '\n';
-	//x[1][1] = 123;
-	//cout << x << '\n';
- }
+void f(matriz<int> *x) {
+    cout << *x << '\n';
+    //x[1][1] = 123;
+    //cout << x << '\n';
+}
 
 int main() {
     matriz<int> a(2, 2);
-	matriz<char> c(2, 2);
+	  matriz<char> c(2, 2);
 
-	c.printDimensions();
+	  c.printDimensions();
     cout << "Matriz inicializada con el constructor de clase" << endl;
     c.printMatrix();
     cout << endl;
 
-	// Codigo Pablo: aca rellena las dos matrices, a y c.
-	int i, j;
-	for(i=0; i<2; i++) {
-	    for(j=0; j<2; j++) {
-	 	    a[i][j] = i+j;
-			c[i][j] = 'A'+i+j;
-	    }
-	}
+	  // Codigo Pablo: aca rellena las dos matrices, a y c.
+	  int i, j;
+	  for(i=0; i<2; i++) {
+	      for(j=0; j<2; j++) {
+	 	        a[i][j] = i+j;
+			      c[i][j] = 'A'+i+j;
+	      }
+	  }
 
-	cout << "Matriz rellena con otro contenido diferente" << endl;
-	c.printMatrix();
+    cout << "Matriz rellena con otro contenido diferente" << endl;
+    c.printMatrix();
     cout << endl;
     cout << endl;
 
-	cout << "Output de la funcion f" << endl;
-	f(&a);
-	//cout << a << '\n';
-	//cout << c << '\n';
+    cout << "Output de la funcion f" << endl;
+    f(&a);
+    //cout << a << '\n';
+    //cout << c << '\n';
 
-	return 0;
+    return 0;
 }
